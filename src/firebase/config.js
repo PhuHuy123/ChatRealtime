631d2c176cf0ae6db602400c8e1eac28aeec796f
PhuHuy123 <nguyenphuhuy180920@gmail.com>
2023-02-15 10:02:20 +0700

    fix deploy

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -24,10 +24,10 @@ firebase.analytics();
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-auth.useEmulator('http://localhost:9099')
-if(window.location.hostname === 'localhost'){
-  db.useEmulator('localhost', '8080');
-}
+// auth.useEmulator('http://localhost:9099')
+// if(window.location.hostname === 'localhost'){
+//   db.useEmulator('localhost', '8080');
+// }
 
 export {db, auth}
 export default firebase;
